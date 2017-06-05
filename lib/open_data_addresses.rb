da1186bb256fc03969000771ff4e402119017353
Jim X. Lin <miljinx@gmail.com>
2017-06-04 23:52:01 -0700

    Add ::address and ::addresses

--- lib/open_data_addresses.rb
+++ lib/open_data_addresses.rb
@@ -1,5 +1,63 @@
 require "open_data_addresses/version"
 
 module OpenDataAddresses
-  # Your code goes here...
+  def self.address(city='SF')
+    address = ''
+    csv = get_csv(city);
+    address_line = rand(2..csv[:lines])
+
+    line_num = 1
+    IO.foreach(csv[:file]) do |line|
+      line_num += 1
+      next unless line_num == address_line
+      address = line.strip
+      break
+    end
+
+    address
+  end
+
+  def self.addresses(amount, city='SF')
+    addresses = []
+    csv = get_csv(city)
+    address_lines = uniq_rand_nums(amount, 2, csv[:lines]).sort
+
+    address_line = address_lines.shift
+    line_num = 1
+    IO.foreach(csv[:file]) do |line|
+      line_num += 1
+      if line_num == address_line
+        addresses << line.strip
+        address_line = address_lines.shift
+      end
+      break if addresses.length == amount
+    end
+
+    addresses
+  end
+
+  private
+
+  def self.get_csv(city)
+    files = {
+      'SF'  => 'lib/addresses/sanfrancisco_registered_business_locations.csv',
+      'NYC' => 'lib/addresses/newyorkcity_legally_operating_businesses.csv'
+    }
+    lines = File.open(files[city], &:readline).to_i;
+
+    {file: files[city], lines: lines}
+  end
+
+  def self.uniq_rand_nums(amount, min, max)
+    raise 'Invalid arguments' if max - min + 1 < amount
+    nums = amount.times.map { rand(min..max) }
+    return nums if nums.uniq.length == amount
+
+    nums.uniq!
+    while nums.length < amount do
+      num = rand(min..max)
+      nums << num unless nums.include?(num)
+    end
+    nums
+  end
 end
--- spec/open_data_addresses_spec.rb
+++ spec/open_data_addresses_spec.rb
@@ -5,7 +5,26 @@ RSpec.describe OpenDataAddresses do
     expect(OpenDataAddresses::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  let(:cities) { ['SF', 'NYC'] }
+  let(:address_regex) { /[\w\s]+,[A-Za-z\s]+,[A-Z][A-Z],\d\d\d\d\d/ }
+  let(:amount) { 10 }
+
+  describe '::address' do
+    it 'returns a random address' do
+      cities.each do |city|
+        address = OpenDataAddresses::address(city)
+        expect(address).to match(address_regex)
+      end
+    end
+  end
+
+  describe '::addresses' do
+    it 'returns an array of addresses' do
+      cities.each do |city|
+        addresses = OpenDataAddresses::addresses(amount, city)
+        expect(addresses.length).to eq(amount)
+        addresses.each {|address| expect(address).to match(address_regex)}
+      end
+    end
   end
 end
